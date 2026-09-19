7c457a599c967440ff7d60c48fd720047e9cc2c3
Raul Portugal <rportugal@outlook.com>
2026-09-18 21:24:54 -0500

    ya subi mi proyecto a github

---
+++ src/main/java/com/proyecto/pooii/PooiiApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.pooii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/pooii/PooiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.pooii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
